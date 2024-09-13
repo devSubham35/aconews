d045dcb9f7a21373447b0ad8363823bb6d07214b
devSubham35 <subhambetal35@gmail.com>
2024-09-13 23:37:03 +0530

    api key added

--- src/hooks/useNewsHook.js
+++ src/hooks/useNewsHook.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_KEY = 'cb3c23598bd98b2ff2a7ce1130b71951'
+const API_KEY = '6013f45648e875fb30bcf2790220fa3b'
 
 const useNewsHook = (category = 'general', language = 'en', country = 'us', searchQuery = '') => {
   const [news, setNews] = useState([]);
